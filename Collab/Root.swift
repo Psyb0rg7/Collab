658be8370fe3e84670e66e628d3ecbe01a021ef4
M F <legocuber@gmail.com>
2018-02-24 15:26:21 -0500

    Added UIPageViews
    added swipeable pages

---
+++ Collab/Root.swift
@@ -0,0 +1,60 @@
+//
+//  Root.swift
+//  Collab
+//
+//  Created by Fredrik Fatemi on 2/24/18.
+//  Copyright © 2018 FPAApps. All rights reserved.
+//
+
+import UIKit
+
+class Root: UIPageViewController, UIPageViewControllerDataSource {
+    lazy var viewControllerList: [UIViewController] = {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc1 = sb.instantiateViewController(withIdentifier: "TextVC")
+        let vc2 = sb.instantiateViewController(withIdentifier: "BlueVC")
+        
+        return [vc1, vc2]
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return nil}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return nil}
+        
+        guard viewControllerList.count > nextIndex else {
+            return nil
+        }
+        
+        return viewControllerList[nextIndex]
+    }
+}
