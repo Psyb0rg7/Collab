c38d9bbfb19d33db49a85aeb84c1577db915443b
Psyborg <kevinchung888@yahoo.com>
2018-02-28 19:27:11 -0500

    Fixed ViewController Stuff
    Fixed stuff

--- Collab/DataEntryViewController.swift
+++ Collab/DataEntryViewController.swift
@@ -13,20 +13,9 @@ class DataEntryViewController: UIViewController {
     @IBOutlet weak var input: UITextField!
     
     @IBAction func submitData(_ sender: Any) {
-        output.text = input.text
         UserDefaults.standard.set(input.text, forKey: "FavoriteColor")
         input.text = ""
-    }
-    
-    @IBAction func clearData(_ sender: Any) {
-        UserDefaults.standard.removeObject(forKey: "FavoriteColor")
-        output.text = "What is your favorite color?"
-    }
-    
-    @IBAction func nextVC(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let entryViewController = storyboard.instantiateViewController(withIdentifier: "AddEntry")
-        self.present(entryViewController, animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -38,12 +27,6 @@ class DataEntryViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if let data = UserDefaults.standard.object(forKey: "FavoriteColor") as? String {
-            output.text = data
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
