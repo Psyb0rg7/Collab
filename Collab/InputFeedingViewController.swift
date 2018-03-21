2413430ab91d43860e73b4b33010022e31884fe8
M F <legocuber@gmail.com>
2018-03-20 20:24:54 -0400

    Feeding input entry

---
+++ Collab/InputFeedingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InputFeedingViewController.swift
+//  Collab
+//
+//  Created by Fredrik Fatemi on 3/20/18.
+//  Copyright © 2018 FPAApps. All rights reserved.
+//
+
+import UIKit
+
+class InputFeedingViewController: UIViewController {
+
+    @IBAction func dateinput(_ sender: Any) {
+    }
+    @IBOutlet weak var comments: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
